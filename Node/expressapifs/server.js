24f28e90375c4d1c7f91cef5ed1581472ba0872d
Esteban <eechava6@eafit.edu.co>
2020-01-02 18:51:38 -0500

    Update: search user by id

--- Node/expressapifs/server.js
+++ Node/expressapifs/server.js
@@ -11,17 +11,15 @@ let app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/registerUser', (req,res) => {    
-    console.log('POST')
     const { body } = req
     const user = JSON.stringify(body)
     db.addUser(user)
     res.sendStatus(200)
 })
 
-app.post('/searchUser', (req,res) => {
-    console.log('GET')
-    var user = db.searchUser(req.body.id, res)
-    res.send(user)
+app.post('/searchUser/:id', (req,res) => {
+    var user = db.searchUser(req.params.id, res)
+    res.send(JSON.parse(user))
 })
 
 app.listen(port, function () {
