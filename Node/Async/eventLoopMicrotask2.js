3ffa84ed7ff2a3c04ac0709edbd0be01099b013f
eechava6 <eechava6@eafit.edu.co>
2020-03-26 00:07:45 -0500

    Add: microtask explanation

--- Node/Async/eventLoopMicrotask2.js
+++ Node/Async/eventLoopMicrotask2.js
@@ -12,5 +12,8 @@ function task2(){
     console.log('task 2');
 }
 
+//Since we are calling from Javascript code, Eventloop won't be empty and will
+//finish the script, if there would be a button listener, after adding 1 listener
+//the task stack would be empty and it will execute Microtask 1
 task1();
 task2();
