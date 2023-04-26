4589d245c01ff58a7097c65c6b57a9f0ab4b982b
eechava66 <esteban.ec123@gmail.com>
2023-04-26 08:58:47 -0500

    adding typescript examples

---
+++ Typescript/src/other_types.ts
@@ -0,0 +1,10 @@
+/**
+ * Types can also be used for primitives, unions and tuples
+ * unlike interfcaes
+ */
+
+type Name = string
+
+type OneOrTwo = {one : boolean} | {two : boolean}
+
+type Data = [number, string]
